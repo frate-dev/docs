f6c2f5f2c055bf3120c69251fce3dfd2109d1fc6
lucas sprouole <lucas.sproule.42@gmail.com>
2023-12-03 22:29:55 -0500

    lib ultraviolet vudu

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
-
+import Logo  from './assets/logo.png'
 const config: DocsThemeConfig = {
-  logo: <span>Frate</span>,
+  logo: <div style={{display:"flex", alignItems:"center", gap:"1rem", fontSize:"1.25rem", fontWeight:"bold"}}><img style={{objectFit:"contain", width:"3rem"}} src={Logo.src} alt="Frate Logo" />Frate</div>,
   project: {
     link: 'https://github.com/frate-dev/frate',
   },
   chat: {
     link: 'https://discord.gg/GdXuH45jpu',
   },
-  docsRepositoryBase: 'https://github.com/frate-dev/template',
+  docsRepositoryBase: 'https://github.com/frate-dev/template/tree/main',
   footer: {
     text: 'Frate Docs',
   },
