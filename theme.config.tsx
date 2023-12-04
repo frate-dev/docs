30d601857d869c58988705b304953d56af800e98
lucas sprouole <lucas.sproule.42@gmail.com>
2023-12-03 21:05:45 -0500

    lib ultraviolet vudu

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Frate</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/frate-dev/frate',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/GdXuH45jpu',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/frate-dev/template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Frate Docs',
   },
 }
 
